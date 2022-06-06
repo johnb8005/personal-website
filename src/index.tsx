e7a86b4d6077507432ae96b628a5b8b0aab9932f
John B <john@nexys.ch>
2022-06-06 23:38:01 +0200

    vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./app";
 
-ReactDOM.render(<App />, document.getElementById("wrapper"));
+ReactDOM.createRoot(document.getElementById("wrapper")!).render(<App />);
