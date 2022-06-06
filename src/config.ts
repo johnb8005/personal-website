913c44a14eb0946e48d16bf4974fe8fa24531743
John B <john@nexys.ch>
2022-06-06 23:46:53 +0200

    config env var to vite

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,5 @@
-export const version =
-  import.meta.env.SNOWPACK_PUBLIC_VERSION || "unset_version";
-export const sha = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA || "unset_sha";
-export const basename = import.meta.env.SNOWPACK_PUBLIC_URL;
+export const version = import.meta.env.VITE_VERSION || "unset_version";
+export const sha = import.meta.env.VITE_GIT_SHA || "unset_sha";
 
 export const ghUrl = "https://github.com/johnb8005/personal-website";
 
